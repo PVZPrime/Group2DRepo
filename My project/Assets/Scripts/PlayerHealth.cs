ea648d4b77a879e02fa8e1495ce4f0fdea6f27c9
Devin <mcmanusdevin2008@gmail.com>
2024-10-09 12:50:25 -0700

    added some scripts

---
+++ My project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    float health = 10;
+    [SerializeField]
+    string levelToLoad = "Lose";
+    float maxHP;
+    [SerializeField]
+    Image healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHP = health;
+        healthBar.fillAmount = health / maxHP;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        //IF we hit an enemy, reduce player hp
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet")
+        {
+            health -= 1;
+            healthBar.fillAmount = health / maxHP;
+            //add consequences
+            //IF health gets too low, reload the current level
+            if (health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                //SceneManager.LoadScene(levelToLoad);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet")
+        {
+            health -= 1;
+            healthBar.fillAmount = health / maxHP;
+            //add consequences
+            //IF health gets too low, reload the current level
+            if (health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                //SceneManager.LoadScene(levelToLoad);
+            }
+        }
+    }
+}
