d16b3d86308a824fb1bf83c171de1d69ce9ca621
Devin <mcmanusdevin2008@gmail.com>
2024-10-11 12:31:38 -0700

    updated title screen

--- My project/Assets/Scripts/HowToPlay.cs
+++ My project/Assets/Scripts/HowToPlay.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class HowToPlay : MonoBehaviour
 {
     [SerializeField]
-    string levelToLoad = "HowToPlay";
+    string levelToLoad = "How To Play";
     // Start is called before the first frame update
     void Start()
     {
