3ce30d36738dd49500cd80ebb38dd2543b918c93
Devin <mcmanusdevin2008@gmail.com>
2024-10-29 12:24:12 -0700

    started final boss arena and script

---
+++ My project/Assets/Scripts/ToBossArena.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToBossArena : MonoBehaviour
+{
+    [SerializeField]
+    string levelToLoad = "DragonLair";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        //IF I hit the boss arena wall, I go to the boss arena
+        if (collision.gameObject.tag == "BossArena")
+        {
+
+                SceneManager.LoadScene(levelToLoad);
+            
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}
